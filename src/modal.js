d29e49256fb4c384448a688cc935e9e7af86b921
Sofia <sofiakucmida@gmail.com>
2026-02-06 15:18:04 +0200

    Favorite-Artist

---
+++ src/modal.js
@@ -0,0 +1,48 @@
+const modalBackdrop = document.querySelector('.artists-modal-backdrop');
+const modalWindow = document.querySelector('.artists-modal-window');
+const modalCloseBtn = document.querySelector('.artists-modal-window-btn');
+const modalContent = document.querySelector('.artists-modal-window-content');
+const loader = document.querySelector('.loader');
+
+export const showLoader = () => {
+  loader.classList.remove('is-hidden-modal-loader');
+};
+
+export const hideLoader = () => {
+  loader.classList.add('is-hidden-modal-loader');
+};
+
+document.addEventListener('click', e => {
+    const btn = e.target.closest('.artist__card--btn');
+    if (!btn) {
+        return
+    }
+    const artistsIdOpenModal = btn.dataset.id;
+    openArtistsModal(artistsIdOpenModal);
+})
+
+ export const openArtistsModal = artistsIdOpenModal => {
+    modalBackdrop.classList.remove('is-hidden-modal');
+    document.body.style.overflow = 'hidden';
+
+    showLoader()
+ }
+
+export const closeArtistModal = () => {
+    modalBackdrop.classList.add('is-hidden-modal');
+    document.body.style.overflow = '';
+
+    hideLoader();
+}
+
+modalCloseBtn.addEventListener('click', closeArtistModal);
+modalBackdrop.addEventListener('click', ev => {
+  if (ev.target === modalBackdrop) {
+    closeArtistModal();
+  }
+});
+document.addEventListener('keydown', ev => {
+  if (ev.key === 'Escape') {
+    closeArtistModal();
+  }
+});
